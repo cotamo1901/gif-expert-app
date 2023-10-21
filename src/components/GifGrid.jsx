c69777d34a50cba82f7037ed673d33a32cd773ed
Alex <jcotamomoli@uniminuto.edu.co>
2023-10-20 19:52:05 -0500

    se va agregar los gifs

--- src/components/GifGrid.jsx
+++ src/components/GifGrid.jsx
@@ -1,31 +1,21 @@
-import { useEffect, useState } from "react";
-import { getGifs } from "../helpers/getGifs";
 import { GifItem } from "./GifItem";
+import { useFetchGifs } from "../hooks/useFetchGifs";
 
 export const GifGrid = ({ category }) => {
-  const [images, setImages] = useState([]);
-
-  const getImages = async () => {
-    const newImages = await getGifs(category);
-    setImages(newImages);
-  };
-
-  useEffect(() => {
-    getImages(category);
-  }, []);
+  const { images, isLoading } = useFetchGifs(category);
 
   return (
     <>
       <h3>{category}</h3>
+      {
+        isLoading ? (
+          <h2>Loading....</h2>
+
+        ):null
+      }
       <div className="card-grid">
-        {images.map(( image ) => (
-          <GifItem 
-          key={image.id}
-          {
-            ...image
-          }
-          
-          />
+        {images.map((image) => (
+          <GifItem key={image.id} {...image} />
         ))}
       </div>
     </>
